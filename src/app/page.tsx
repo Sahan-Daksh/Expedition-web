4b70c137c49c5263d32441c75a86fa73e19121b7
Sahan Dakshitha <sahandakshitha45@gmail.com>
2024-10-14 12:35:36 +0530

    CTAs and New Timeline Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,22 @@ import { benefitOne} from "@/components/data";
 import Contact from "@/components/Contact";
 import About from "@/components/About";
 import Timeline from "@/components/Timeline";
+import { Cta } from "@/components/Cta";
+import Phases from "@/components/TimelineNew";
 export default function Home() {
   return (
     <Container>
       <Hero />
       <About />
+      <SectionTitle title="Get Involved">
+      Dive into the excitement—become a part of the Expedition journey!
+      </SectionTitle>
+      <Cta />
       <Timeline />
+      <SectionTitle title="Timeline">
+      Explore our schedule and see what we have planned for the Expedition series!
+      </SectionTitle>
+      <Phases/>
       <StartupBanner />
       
       {/* <SectionTitle title="About Expedition">
